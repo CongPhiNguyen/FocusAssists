7ee79e4a7b6539a25a39a3265b6a1b13db75042f
Quang Hoàng Dận <19522092@ms.uit.edu.vn>
2021-06-28 23:37:19 +0700

    update mainscreen

--- focus_assists/lib/pages/focusAssist.dart
+++ focus_assists/lib/pages/focusAssist.dart
@@ -1,116 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:focus_assist/pages/progress_screen.dart';
-import 'package:focus_assist/pages/setting_screen.dart';
-import 'package:focus_assist/pages/journal_screen.dart';
-// import 'package:focus_assist/classes/DbProvider.dart';
-// import 'package:focus_assist/classes/Data.dart';
 
-import 'journal_screen.dart';
+import 'package:focus_assist/classes/Data.dart';
+import 'package:focus_assist/classes/theme_provider.dart';
+import 'package:focus_assist/pages/login/screen/lock_screen.dart';
+import 'package:focus_assist/pages/main_screen.dart';
+import 'package:provider/provider.dart';
+import 'login/screen/welcome_screen.dart';
 
-//import 'dart:math';
-
-class FocusAssist extends StatefulWidget {
+class FocusAssist extends StatelessWidget {
   @override
-  _FocusAssistState createState() => _FocusAssistState();
-}
-
-class _FocusAssistState extends State<FocusAssist> {
-  int selectedIndex = 4;
-  //final dbHelper = DbProvider.instance;
-  //List<Map<String, dynamic>> database;
-
-  // String getRandomString(int len) {
-  //   var r = Random();
-  //   return String.fromCharCodes(
-  //       List.generate(len, (index) => r.nextInt(33) + 89));
-  // }
-
-  // @override
-  // void initState() async {
-  //   // TODO: implement initState
-  //   super.initState();
-  //   database = await dbHelper.query('NGUOIDUNG');
-  //   while (database.length != 0) {
-  //     Map<String, dynamic> row = {
-  //       'MANGUOIDUNG': getRandomString(5),
-  //       'TENTAIKHOAN': 'NGUYEN CONG PHI',
-  //       'MATKHAU': '1111',
-  //     };
-  //     final id = await dbHelper.insert('NGUOIDUNG', row);
-  //     print('inserted row id: $id');
-  //     database = await dbHelper.query('NGUOIDUNG');
-  //   }
-  //   String name = database[0]['MANGUOIDUNG'];
-  //   StaticData.userID = name;
-  // }
-
-  final List<Widget> widgetOptions = <Widget>[
-    Center(
-      child: Text(
-        'Focus Timer',
-      ),
-    ),
-    Center(child: JournalScreen()),
-    Center(
-      child: Text(
-        'Reward',
-      ),
-    ),
-    /*Center(
-      child: Text(
-        'Progress',
-      ),
-    ),*/
-    ProgressScreen(),
-    SettingScreen(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: widgetOptions.elementAt(selectedIndex),
-        bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: Colors.blue,
-          unselectedItemColor: Colors.grey[600],
-          iconSize: 28.0,
-          backgroundColor: Colors.grey[300],
-          currentIndex: selectedIndex,
-          onTap: onItemTapped,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.timer,
-              ),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.wysiwyg_rounded),
-              label: 'Journal',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.star),
-              label: 'Farm',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.waterfall_chart),
-              label: 'Progress',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: 'Settings',
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void onItemTapped(int index) {
-    setState(() {
-      selectedIndex = index;
-    });
-  }
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    builder: (context, _) {
+      final themeProvider = Provider.of<ThemeProvider>(context);
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Focus Assist',
+        themeMode: themeProvider.themeMode,
+        theme: MyThemes.lightTheme,
+        darkTheme: MyThemes.darkTheme,
+        home: (StaticData.isSignedIn)?((StaticData.isPrivacyLockOn)?LockScreen():MainScreen()):WelcomeScreen(),
+      );
+    },
+  );
 }
