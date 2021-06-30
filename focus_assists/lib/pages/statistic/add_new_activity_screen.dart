2adc495dadc3b05f6fabf4f2775754dc03148a2e
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2021-06-30 22:59:47 +0700

    Sửa lỗi nhập sai ngày

--- focus_assists/lib/pages/statistic/add_new_activity_screen.dart
+++ focus_assists/lib/pages/statistic/add_new_activity_screen.dart
@@ -76,41 +76,37 @@ class _AddNewState extends State<AddNew> {
                 softWrap: true,
               ),
             ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 8, 8, 8),
-                child: Center(
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                        width: 50,
-                        child: TextField(
-                          keyboardType: TextInputType.number,
-                          controller: getDayPerWeek,
-                          textAlign: TextAlign.center,
-                          decoration: InputDecoration(
-                            hintText: 'days',
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.grey),
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.grey),
-                            ),
-                          ),
-                          style: TextStyle(fontSize: 20),
-                          inputFormatters: <TextInputFormatter>[
-                            FilteringTextInputFormatter.digitsOnly
-                          ],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 8, 8, 8),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    width: 50,
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      controller: getDayPerWeek,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        hintText: 'days',
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
                         ),
                       ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text("per week", style: TextStyle(fontSize: 20))
-                    ],
+                      style: TextStyle(fontSize: 20),
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.digitsOnly
+                      ],
+                    ),
                   ),
-                ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("per week", style: TextStyle(fontSize: 20))
+                ],
               ),
             ),
           ],
@@ -156,8 +152,6 @@ class _AddNewState extends State<AddNew> {
                             width: 48.5,
                             decoration: BoxDecoration(
                               color: checkDay[index]
-                                  // ? Color(0xff8A2BE2)
-                                  // : Color(0xffF0FFF0),
                                   ? (!StaticData.isDarkMode)
                                       ? Colors.grey[50]
                                       : Colors.grey[700]
@@ -174,21 +168,6 @@ class _AddNewState extends State<AddNew> {
                                             ? Colors.blue
                                             : Colors.grey[500],
                                     width: 5),
-                                // top: BorderSide(
-                                //     color: !checkDay[index]
-                                //         ? (!StaticData.isDarkMode)?Colors.blue:Colors.grey[700]
-                                //         : (!StaticData.isDarkMode)?Colors.grey[200]:Colors.grey[500],
-                                //     width: 1),
-                                // right: BorderSide(
-                                //     color: !checkDay[index]
-                                //         ? (!StaticData.isDarkMode)?Colors.blue:Colors.grey[700]
-                                //         : (!StaticData.isDarkMode)?Colors.grey[200]:Colors.grey[500],
-                                //     width: 1),
-                                // left: BorderSide(
-                                //     color: !checkDay[index]
-                                //         ? (!StaticData.isDarkMode)?Colors.blue:Colors.grey[700]
-                                //         : (!StaticData.isDarkMode)?Colors.grey[200]:Colors.grey[500],
-                                //     width: (index == 0) ? 1 : 0)
                               ),
                             ),
                             child: Center(
@@ -196,8 +175,6 @@ class _AddNewState extends State<AddNew> {
                                   style: TextStyle(
                                     fontSize: 12,
                                     color: !checkDay[index]
-                                        // ? Color(0xff8A2BE2)
-                                        // : Color(0xffF0FFF0))
                                         ? Colors.black
                                         : Colors.black,
                                   )),
@@ -519,7 +496,7 @@ class _AddNewState extends State<AddNew> {
 
   @override
   Widget build(BuildContext context) {
-    OutlineInputBorder k = OutlineInputBorder(
+    OutlineInputBorder borderStyle = OutlineInputBorder(
       borderRadius: BorderRadius.all(Radius.circular(4)),
       borderSide: BorderSide(
           width: 1,
@@ -587,25 +564,18 @@ class _AddNewState extends State<AddNew> {
                         textAlign: TextAlign.center,
                         decoration: InputDecoration(
                           alignLabelWithHint: true,
-                          border: OutlineInputBorder(
-                              // borderRadius: BorderRadius.circular(8.0),
-                              // borderSide: BorderSide(
-                              //   color: Colors.black,
-                              // ),
-                              ),
                           labelText: 'Activity Name',
                           labelStyle: TextStyle(
                               color: (!StaticData.isDarkMode)
                                   ? Colors.black
                                   : Colors.grey[400],
                               fontSize: 18),
-                          focusedBorder: k,
-                          disabledBorder: k,
-                          enabledBorder: k,
-                          errorBorder: k,
-                          focusedErrorBorder: k,
+                          focusedBorder: borderStyle,
+                          disabledBorder: borderStyle,
+                          enabledBorder: borderStyle,
+                          errorBorder: borderStyle,
+                          focusedErrorBorder: borderStyle,
                         ),
-                        // style: TextStyle(fontSize: 20, color: Colors.white),
                       ),
                       SizedBox(
                         height: 10,
@@ -616,17 +586,11 @@ class _AddNewState extends State<AddNew> {
                         decoration: InputDecoration(
                             isDense: true,
                             contentPadding: EdgeInsets.all(8),
-                            focusedBorder: k,
-                            disabledBorder: k,
-                            enabledBorder: k,
-                            errorBorder: k,
-                            focusedErrorBorder: k,
-                            border: OutlineInputBorder(
-                                // borderRadius: BorderRadius.circular(8.0),
-                                // borderSide: BorderSide(
-                                //   color: Colors.grey,
-                                // ),
-                                ),
+                            focusedBorder: borderStyle,
+                            disabledBorder: borderStyle,
+                            enabledBorder: borderStyle,
+                            errorBorder: borderStyle,
+                            focusedErrorBorder: borderStyle,
                             labelText: 'Description (optional)',
                             labelStyle: TextStyle(
                                 color: (!StaticData.isDarkMode)
@@ -634,7 +598,6 @@ class _AddNewState extends State<AddNew> {
                                     : Colors.grey[400],
                                 fontSize: 18)),
                         maxLines: 3,
-                        // style: TextStyle(fontSize: 20, color: Colors.white),
                       ),
                       SizedBox(
                         height: 10,
@@ -804,7 +767,6 @@ class _AddNewState extends State<AddNew> {
                     : (dropDownValue == 'Flexible')
                         ? Flexible()
                         : Repeating()),
-            //debugWidget(),
           ],
         ));
   }
